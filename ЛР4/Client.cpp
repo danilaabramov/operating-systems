0a308abb0ce266fcdf9b1b03f6a1e6386b0c895a
Danila Abramov <67269761+danilaabramov@users.noreply.github.com>
2023-07-30 22:34:17 +0400

    Add files via upload

---
+++ ЛР4/Client.cpp
@@ -0,0 +1,42 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+#pragma warning(disable: 4996)
+#include <winsock2.h>
+#include <iostream>
+#include "conio.h"
+
+using namespace std;
+
+int main() {
+	// загружаем WSAStartup
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	WSAData wsaData; // создаем структуру для загрузки
+	WORD word = MAKEWORD(2, 1);
+	if (WSAStartup(word, &wsaData) != 0) { // проверка на подключение библиотеки
+		cout << "Error: ошибка подключения библиотеки\n";
+		WSACleanup();
+		return 0;
+	}
+	SOCKADDR_IN sockAddr; // получаем информацию об адресе сокета
+	sockAddr.sin_addr.s_addr = inet_addr("127.1.1.1"); // адрес
+	sockAddr.sin_port = htons(1111); // порт
+	sockAddr.sin_family = AF_INET; // семейство протоколов
+	SOCKET Connection = socket(AF_INET, SOCK_STREAM, 0); // создаем сокет для соединения с сервером
+	if (connect(Connection, (SOCKADDR*)&sockAddr, sizeof(sockAddr)) != 0) cout << "Error: Ошибка подключения\n";
+	else {
+		cout << "Подключение произошло успешно!\n";
+		char str[15951];
+	
+		for (int i = 0; i < 15951; ++i) {
+			char c;
+			str[i] = getch();
+			if (str[i] == '\r') cout << endl;
+			if (str[i] == '\b') cout << "\b\ ";
+			c = str[i];
+			send(Connection, &c, 1, 0);
+			cout << str[i];
+		}
+	}
+	WSACleanup(); // деинициализируем библиотеки
+	closesocket(Connection); // закрываем сокет
+}
---
+++ ЛР4/Server.cpp
@@ -0,0 +1,42 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+#pragma warning(disable: 4996)
+#include <winsock2.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// загружаем WSAStartup
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	WSAData wsaData; //создаем структуру для загрузки
+	WORD word = MAKEWORD(2, 1);
+	if (WSAStartup(word, &wsaData) != 0) { //проверка на подключение библиотеки
+		cout << "Error: ошибка подключения библиотеки\n";
+		WSACleanup();
+		return 0;
+	}
+	SOCKADDR_IN sockAddr;//получаем информацию об адресе сокета
+	int sizeOfSockAddr = sizeof(sockAddr); //размер
+	sockAddr.sin_addr.s_addr = inet_addr("127.1.1.1"); //адрес
+	sockAddr.sin_port = htons(1111); //порт
+	sockAddr.sin_family = AF_INET; //семейство протоколов
+	SOCKET soketListen = socket(AF_INET, SOCK_STREAM, 0);//сокет для прослушивания порта
+	bind(soketListen, (SOCKADDR*)&sockAddr, sizeof(sockAddr));//функция для привязки сокета
+	listen(soketListen, SOMAXCONN);//прослушивание, сколько запросов ожидается
+	SOCKET Connection = accept(soketListen, (SOCKADDR*)&sockAddr, &sizeOfSockAddr); // подключается клиент
+	if (Connection == 0)  cout << "Error: Ошибка подключения\n"; //не смогли подключиться
+	else {
+		cout << "Клиент подключен!\n";
+		for (int i = 0; i < 15951; ++i) {
+			char c;
+			if (recv(Connection, &c, 1, 0) == -1) break;
+			if (c == '\r') cout << endl;
+			if (c == '\b') cout << "\b\ ";
+			cout << c;	
+		}
+	}
+	WSACleanup(); //деинициализируем библиотеки
+	closesocket(Connection); //закрываем сокет
+	closesocket(soketListen); //закрываем сокет
+}
